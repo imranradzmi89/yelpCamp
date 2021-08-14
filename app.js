5524353be5e6a6374da4de8288a0d55080be2ffc
unknown <imranradzmi89@gmail.com>
2021-08-14 12:07:10 +0800

    corrected server address

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const User = require('./models/user.js');
 //removes Mongo injections
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet');
-const dbURL = 'mongodb://localhost:27017/yelp-camp'
+const dbURL = process.env.DB_URL
 const MongoDBStore = require('connect-mongo');
 
 //import routers
